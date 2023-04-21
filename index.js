f09be286025658634b91dd66b6305195ed56b1c1
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-21 16:41:07 +0200

    Task completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log('funguju!');
+
+const renderMovie = (movies) => {
+  const movieListElm = document.querySelector('.movie-list');
+  movieListElm.innerHTML = movies
+    .map((movie) => {
+      return `
+    <li class="movie-detail">
+          <div class="movie-poster">
+            <img 
+              src="${movie.posterUrl}"
+              alt="${movie.title}"
+            />
+          </div>
+          <div class="movie-info">
+            <h2 class="movie-title">${movie.title}</h2>
+            <div class="movie-year">Rok vydání: ${movie.year}"</div>
+            <div class="movie-link">
+              <a href="${movie.url}"" target="_blank">Odkaz na CSFD</a>
+            </div>
+          </div>
+        </li>
+    `;
+    })
+    .join('');
+}
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => response.json())
+  .then(renderMovie);
