4a403c655f0ccfa56fc9da05a8c4f943545a82b1
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-21 17:09:08 +0200

    Add filter -genre

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('funguju!');
 
-const renderMovie = (movies) => {
+const renderMovies = (movies) => {
   const movieListElm = document.querySelector('.movie-list');
   movieListElm.innerHTML = movies
     .map((movie) => {
@@ -27,4 +27,29 @@ const renderMovie = (movies) => {
 
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
   .then((response) => response.json())
-  .then(renderMovie);
+  .then(renderMovies);
+
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/genres')
+  .then((response) => response.json())
+  .then((data) => {
+    const selectGenre = document.querySelector('#select-genre');
+    selectGenre.innerHTML = data
+      .map((genre) => {
+        return `
+  <option value="${genre}">${genre.charAt(0).toUpperCase() + genre.slice(1)}</option>
+  `;
+      })
+      .join('');
+  })
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const select = document.querySelector('#select-genre');
+  fetch(
+    `https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies?genre=${select.value}`
+  )
+    .then((response) => response.json())
+    .then((renderMovies));
+});
